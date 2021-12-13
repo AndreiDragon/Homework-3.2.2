4421258e01aa47cd3a62a033c692f43b21556aeb
AndreiDragon <Andrei-the-hot@yandex.ru>
2021-12-13 20:27:09 +0400

    It is alive!

---
+++ Rouble for 100/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        long account = 100;
+        long replenish = 2000;
+        long bonus = 0;
+        if (replenish >= 1000) {
+            bonus = bonus + replenish / 100;
+        }
+        System.out.println(account+replenish+bonus);
+        System.out.println(bonus);
+    }
+}
